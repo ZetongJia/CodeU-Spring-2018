2cd7ccfa46ae2f8ab2c79b051ca1c70a2064bb7c
aublhr99 <aublhr99@gmail.com>
2018-06-22 12:34:14 -0400

    Not working testing servlet for activity feed, will work on

--- src/test/java/codeu/controller/ActivityFeedServletTest.java
+++ src/test/java/codeu/controller/ActivityFeedServletTest.java
@@ -1,84 +1,67 @@
 // package codeu.controller;
 //
+// import codeu.model.data.Conversation;
+// import codeu.model.data.Message;
 // import codeu.model.data.User;
+// import codeu.model.store.basic.ConversationStore;
+// import codeu.model.store.basic.MessageStore;
 // import codeu.model.store.basic.UserStore;
 // import java.io.IOException;
 // import java.time.Instant;
+// import java.util.ArrayList;
+// import java.util.List;
 // import java.util.UUID;
-//
 // import javax.servlet.RequestDispatcher;
 // import javax.servlet.ServletException;
 // import javax.servlet.http.HttpServletRequest;
 // import javax.servlet.http.HttpServletResponse;
 // import javax.servlet.http.HttpSession;
+// import org.junit.Assert;
 // import org.junit.Before;
 // import org.junit.Test;
+// import org.mockito.ArgumentCaptor;
 // import org.mockito.Mockito;
 //
 // public class ActivityFeedServletTest {
 //
-//   private LoginServlet loginServlet;
+//   private ActivityFeedServlet activityfeedServlet;
 //   private HttpServletRequest mockRequest;
+//   private HttpSession mockSession;
 //   private HttpServletResponse mockResponse;
 //   private RequestDispatcher mockRequestDispatcher;
-//
+//   private ConversationStore mockConversationStore;
+//   private MessageStore mockMessageStore;
+//   private UserStore mockUserStore;
+// 
 //   @Before
 //   public void setup() {
-//     loginServlet = new LoginServlet();
+//     activityfeedServlet = new ActivityFeedServlet();
+//
 //     mockRequest = Mockito.mock(HttpServletRequest.class);
+//     mockSession = Mockito.mock(HttpSession.class);
+//     Mockito.when(mockRequest.getSession()).thenReturn(mockSession);
+//
 //     mockResponse = Mockito.mock(HttpServletResponse.class);
 //     mockRequestDispatcher = Mockito.mock(RequestDispatcher.class);
-//     Mockito.when(mockRequest.getRequestDispatcher("/WEB-INF/view/login.jsp"))
+//     Mockito.when(mockRequest.getRequestDispatcher("/WEB-INF/view/activityfeed.jsp"))
 //         .thenReturn(mockRequestDispatcher);
+//
+//     mockConversationStore = Mockito.mock(ConversationStore.class);
+//     activityfeedServlet.setConversationStore(mockConversationStore);
+//
+//     mockMessageStore = Mockito.mock(MessageStore.class);
+//     activityfeedServlet.setMessageStore(mockMessageStore);
+//
+//     mockUserStore = Mockito.mock(UserStore.class);
+//     activityfeedServlet.setUserStore(mockUserStore);
 //   }
 //
 //   @Test
 //   public void testDoGet() throws IOException, ServletException {
-//     loginServlet.doGet(mockRequest, mockResponse);
+//     Mockito.when(mockRequest.getRequestURI()).thenReturn("/activityfeed");
+//
+//     activityfeedServlet.doGet(mockRequest, mockResponse);
 //
 //     Mockito.verify(mockRequestDispatcher).forward(mockRequest, mockResponse);
-//   }
-//
-//   @Test
-//   public void testDoPost_NewUser() throws IOException, ServletException {
-//     Mockito.when(mockRequest.getParameter("username")).thenReturn("test username");
-//     Mockito.when(mockRequest.getParameter("password")).thenReturn("test password");
-//
-//     UserStore mockUserStore = Mockito.mock(UserStore.class);
-//     Mockito.when(mockUserStore.isUserRegistered("test username")).thenReturn(false);
-//     loginServlet.setUserStore(mockUserStore);
-//
-//     loginServlet.doPost(mockRequest, mockResponse);
-//
-//     Mockito.verify(mockRequest).setAttribute("error", "That username was not found.");
-//     Mockito.verify(mockRequestDispatcher).forward(mockRequest, mockResponse);
-//   }
-//
-//   @Test
-//   public void testDoPost_ExistingUser() throws IOException, ServletException {
-//
-//     User user =
-//         new User(
-//             UUID.randomUUID(),
-//             "test username",
-//             "$2a$10$.e.4EEfngEXmxAO085XnYOmDntkqod0C384jOR9oagwxMnPNHaGLa",
-//             Instant.now());
-//
-//     Mockito.when(mockRequest.getParameter("username")).thenReturn("test username");
-//     Mockito.when(mockRequest.getParameter("password")).thenReturn("test password");
-//
-//     UserStore mockUserStore = Mockito.mock(UserStore.class);
-//     Mockito.when(mockUserStore.isUserRegistered("test username")).thenReturn(true);
-//     Mockito.when(mockUserStore.getUser("test username")).thenReturn(user);
-//     loginServlet.setUserStore(mockUserStore);
-//
-//     HttpSession mockSession = Mockito.mock(HttpSession.class);
-//     Mockito.when(mockRequest.getSession()).thenReturn(mockSession);
-//
-//     loginServlet.doPost(mockRequest, mockResponse);
-//
-//     Mockito.verify(mockUserStore, Mockito.never()).addUser(Mockito.any(User.class));
-//     Mockito.verify(mockSession).setAttribute("user", "test username");
-//     Mockito.verify(mockResponse).sendRedirect("/conversations");
 //   }
 // }
