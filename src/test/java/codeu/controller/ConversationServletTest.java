9fde7c666b9fd912df4a46c75625e36667c166fa
aublhr99 <aublhr99@gmail.com>
2018-07-19 13:19:13 -0400

    Corrected login

--- src/test/java/codeu/controller/ConversationServletTest.java
+++ src/test/java/codeu/controller/ConversationServletTest.java
@@ -66,6 +66,19 @@ public class ConversationServletTest {
 
   @Test
   public void testDoGet() throws IOException, ServletException {
+    User testUser =
+        new User(
+            UUID.randomUUID(),
+            "testUser",
+            "$2a$10$.e.4EEfngEXmxAO085XnYOmDntkqod0C384jOR9oagwxMnPNHaGLa",
+            Instant.now(),
+            "",
+            false);
+
+    Mockito.when(mockSession.getAttribute("user")).thenReturn("testUser");
+    Mockito.when(mockUserStore.isUserRegistered("testUser")).thenReturn(true);
+    Mockito.when(mockUserStore.getUser("testUser")).thenReturn(testUser);
+    
     List<Conversation> fakeConversationList = new ArrayList<>();
     fakeConversationList.add(
         new Conversation(UUID.randomUUID(), UUID.randomUUID(), "test_conversation", Instant.now()));
